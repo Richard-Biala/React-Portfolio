8595152a826490306d90605741d5da5805b6ecce
Richard <rkj128@gmail.com>
2020-10-01 11:06:22 -0700

    Hero updated

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state ={
-      title: 'Richard Biala',
+      title: 'Fight for the Future',
       headerLinks: [
         {title: 'Home', path: '/'},
         {title: 'About', path: '/about'},
